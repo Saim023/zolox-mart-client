f9f55a6312f6ee3babf7c25c5606f880ee20cfbc
Md. Saim Hossain <saimhossain023@gmail.com>
2025-07-12 23:05:34 +0600

    path updated

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // or vue() if using Vue
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // or vue()
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          // Group React-related dependencies
-          if (
-            id.includes("node_modules/react") ||
-            id.includes("node_modules/react-dom")
-          ) {
-            return "vendor-react";
-          }
-
-          // Group other large dependencies
-          if (id.includes("node_modules")) {
-            return "vendor"; // general vendor chunk
-          }
-
-          // Optional: Create separate chunks for specific features
-          if (id.includes("src/components/")) {
-            return "components";
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Adjust warning limit if needed
-  },
+  plugins: [react()],
 });
